a3b18709e5ff82a544aa26d486644322ab1ad6b1
James Kratus <jimkratus@gmail.com>
2018-07-12 15:32:51 -0400

    email form can modify database. (add, delete, display)

--- email_validation/server.py
+++ email_validation/server.py
@@ -11,34 +11,46 @@ mysql = connectToMySQL('emailValidation')
 
 @app.route('/')
 def index():
-    return render_template('index.html', all_emails = all_emails)
+    return render_template('index.html')
 
 @app.route('/create_email', methods=['POST'])
 def checkEmail():
     timestamp = datetime.strftime(datetime.now(), "%Y/%m/%d %I:%M%p")
     user = {
         'email': request.form['email'],
-        'timestamp': timestamp
-    }    
+    }  
+    duplicate_query = "SELECT * FROM emails WHERE email = %(email)s LIMIT 1;"
+    user_list = mysql.query_db(duplicate_query, user)  
     if not EMAIL_REGEX.match(user['email']) or len(user['email']) < 1:
         flash("Invalid Email Address")
-        print("invald")
-    elif:
-        matches = SELECT id FROM emails WHERE email = {user.email} LIMIT 1;
-        if len(matches) > 0:    
-            flash("Email Address already exists")
-            print("duplicate")
+    elif len(user_list) > 0:   
+        print("************ user list ********* \n", user['email'], user_list) 
+        flash("Email Address already exists")
+        print("duplicate")
     if '_flashes' in session.keys():
         return redirect('/')
     else:
-        query = "INSERT INTO emails (email, created_at, updated_at) VALUES (%(email)s, NOW(), NOW());""
-        user_list = mysql.query_db(query, user)
-        return redirect('/valid_email')
+        input_query = "INSERT INTO emails (email, created_at, updated_at) VALUES (%(email)s, NOW(), NOW());"
+        mysql.query_db(input_query, user)
+        return redirect('/display_email')
 
-@app.route('/valid_email')
-def createEmail():
+@app.route('/display_email')
+def displayEmail():
     all_emails = mysql.query_db("SELECT email, created_at FROM emails")
     return render_template('success.html', all_emails = all_emails)
+
+@app.route('/delete', methods=['POST'])
+def delete_email():
+    entry_to_delete = {
+        'entry': request.form['entry']
+    }
+    delete_query = "DELETE FROM `emailValidation`.`emails` WHERE `email`= %(entry)s;"
+    mysql.query_db(delete_query, entry_to_delete)
+    return redirect('/display_email')
+
+@app.route('/return')
+def return_to_main():
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
