c1334c3357c063a26d79f7076550eae4c74c102d
James Kratus <jimkratus@gmail.com>
2018-07-13 12:44:40 -0400

    connected db to form.

--- email_validation/server.py
+++ email_validation/server.py
@@ -24,9 +24,7 @@ def checkEmail():
     if not EMAIL_REGEX.match(user['email']) or len(user['email']) < 1:
         flash("Invalid Email Address")
     elif len(user_list) > 0:   
-        print("************ user list ********* \n", user['email'], user_list) 
         flash("Email Address already exists")
-        print("duplicate")
     if '_flashes' in session.keys():
         return redirect('/')
     else:
@@ -54,3 +52,54 @@ def return_to_main():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+# from flask import Flask, render_template, session, request, redirect, flash
+# from mysqlconnection import connectToMySQL
+# import re
+# EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+# app = Flask(__name__)
+# app.secret_key = "ThisIsSecret!"
+# @app.route("/")
+# def index():
+#     debugHelp("INDEX METHOD")
+#     return render_template("index2.html")
+   
+# @app.route("/reserve", methods=['POST'])
+# def reserve():
+#     # Let's add validtion rules
+#     if len(request.form['email']) < 1:
+#         flash("Email cannot be blank!", 'email')
+#     elif not EMAIL_REGEX.match(request.form['email']):
+#         flash("Invalid Email Address!", 'email')
+#     if len(request.form['name']) < 1:
+#         flash("Name cannot be blank!", 'name')
+#     elif len(request.form['name']) <= 3:
+#         flash("Name must be 3+ characters", 'name')
+    
+#     if len(request.form['pin']) < 1:
+#         flash("Pin cannot be blank!", 'pin')
+#     elif request.form['pin'].isdigit() == False:
+#         flash("Pin must be numeric", 'pin')
+#     elif len(request.form['pin']) < 4 or len(request.form['pin'])>8:
+#         flash("Pin must be 4-8 digits", 'pin')
+            
+
+#     # return "reserve"
+#     if '_flashes' in session.keys():
+#         return redirect("/")
+#     else:
+#         session["name"] = request.form['name']
+#         return redirect("/success")
+  
+# @app.route("/success")
+# def success():
+#     return "Thank you " + session["name"] + ". Your seat is now reserved!"
+  
+# def debugHelp(message = ""):
+#     print("\n\n-----------------------", message, "--------------------")
+#     print('REQUEST.FORM:', request.form)
+#     print('SESSION:', session)
+# if __name__ == "__main__":
+#     app.run(debug=True)
--- registration_from/server.py
+++ registration_from/server.py
@@ -6,53 +6,84 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 app = Flask(__name__)
 app.secret_key = 'dortmund'
 
+mysql = connectToMySQL('regform_1')
+
 @app.route("/")
-def user_form():   
+def user_form():  
+     
     return render_template("index.html")
 
 @app.route("/success")
-def create_user():
+def user_registered():
     return render_template("success.html")
 
 @app.route("/register", methods=['POST'])
-def info_page():     
+def register_page():
+    user_sub = request.form
+
+    #name validations
+    if user_sub['firstName'].isalpha() == False or len(user_sub['firstName']) <= 2:
+        flash('Invalid first name', 'firstName')
+
+    if user_sub['lastName'].isalpha() == False or len(user_sub['lastName']) <= 2:
+        flash('Invalid last name', 'lastName')
+
+    #email validations    
+    if not EMAIL_REGEX.match(user_sub['email']) or len(user_sub['email']) < 1:
+        flash("Invalid Email Address", "email")
+
+    #password validation
+    if len(user_sub['password']) < 8:
+        flash('Password should be more than 8 characters', 'password')
+
+    if user_sub['password'] != user_sub['confirmPassword']:
+        flash('Passwords should match', 'confirmPassword')
+
+    # registration fail or success
+    if '_flashes' in session.keys():
+        return redirect('/')
+    else:
+        session["name"] = user_sub['firstName']
+        flash("Invalid Email Address", "email")
+
+        insert_query = "INSERT INTO users (first_name, last_name, email, password, created_at, updated_at) VALUES (%(firstName)s, %(lastName)s, %(email)s, %(password)s, NOW(), NOW());"
+        mysql.query_db(insert_query, user_sub)
+        
+        return redirect("/success")     
 
 @app.route("/login", methods=["POST"])
-def create_user():
+def login_user():
+    login_sub = request.form
+    password_match_query = "SELECT id, first_name, email, password FROM users WHERE email = %(email)s AND password = %(password)s;"
+    password_match_query_result = mysql.query_db(password_match_query, login_sub)
+    
+    #user validation success    
+    if password_match_query_result:
+        session["id"] = password_match_query_result[0]['id']
+        session["name"] = password_match_query_result[0]['first_name']
+        session["logged_in"] = True
+        debugHelp('check session')
+        return redirect("/success")
 
+    #user validation fail
+    else:
+        flash("You could not be logged in", "login_error")
+        debugHelp('check session')
+        return redirect('/')     
 
-
-
-
+@app.route("/logout")
+def logout_user():
+    flash('You have been logged out', 'logout')
+    return redirect("/")
 
 def debugHelp(message = ""):
     print("\n\n-----------------------", message, "--------------------")
     print('REQUEST.FORM:', request.form)
     print('SESSION:', session)
+    print("\n\n-------------------------------------------")
 
 if __name__=="__main__":
     app.run(debug=True)  
 
 
 
-    #     #password validation
-    # if len(request.form['Password']) <= 8:
-    #     flash('Password should be more than 8 characters', 'password')
-
-    # if request.form['Password'] != request.form['confirmPassword']:
-    #     flash('Password and Password Confirmation should match', 'password')
-
-    # #name validation
-    # if request.form['firstName'].isalpha() == False:
-    #     flash('Invalid first name provided', 'firstname')
-
-    # if request.form['lastName'].isalpha() == False:
-    #     flash('Invalid last name provided', 'lastname')
-
-    # if not EMAIL_REGEX.match(request.form['email']) or len(request.form['email']) < 1:
-    #     flash("Invalid Email Address", "email")
-        
-    # if '_flashes' in session.keys():
-    #     return redirect('/')
-    # else:
-    #     return render_template('result.html', info=session['info'])
